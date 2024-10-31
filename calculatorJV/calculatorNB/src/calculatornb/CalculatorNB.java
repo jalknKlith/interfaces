61ae04b3a055f355d5a0bbc7abb6cd32b973eab4
jalknKlith <alejalkndro@gmail.com>
2024-10-30 19:42:51 -0500

    calculator

---
+++ calculatorJV/calculatorNB/src/calculatornb/CalculatorNB.java
@@ -0,0 +1,13 @@
+package calculatornb;
+import forms.Calculator;
+
+public class CalculatorNB {
+
+    public static void main(String[] args) {
+        
+        Calculator obj = new Calculator();
+        obj.setVisible(true);
+        
+    }
+    
+}
---
+++ calculatorJV/src/calculatorjava/CalculatorJAVA.java
@@ -0,0 +1,7 @@
+public class CalculatorJAVA {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
